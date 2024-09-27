004ee67a1b55d35d8bce1d5c902d04554784d4b0
raouf <rauofallama@gmail.com>
2024-09-27 15:32:01 -0700

    adding Fake Authentication Implementing Login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -10,9 +10,9 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'login':
-      return {...state, user: action.payload, isAuthenticated: true}
+      return { ...state, user: action.payload, isAuthenticated: true }
     case 'logout':
-      return {...state, user: null, isAuthenticated: false}
+      return { ...state, user: null, isAuthenticated: false }
     default: throw new Error('Unknown action')
   }
 }
@@ -23,24 +23,28 @@ const FAKE_USER = {
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
 
-function AuthProvider ({children}) {
-  const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState)
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(reducer, initialState)
 
 
-  function login({email, password}) {
-    if (email === FAKE_USER.email && password === FAKE_USER.password){
-      dispatch({type: "login", payload: FAKE_USER})
+  function login( email, password ) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER })
+      console.log(email, password)
     }
   }
   function logout() {
-    dispatch({type: 'logout'})
+    dispatch({ type: 'logout' })
   }
-  return <AuthContext.Provider value={{user, isAuthenticated, login, logout}}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>{children}</AuthContext.Provider>
 }
 
 function useAuth() {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
+
   if (context === undefined) throw new Error("AuthContext was used okutside AuthProvider")
+
+  return context
 }
 
-export {AuthProvider, useAuth}
+export { AuthProvider, useAuth }
